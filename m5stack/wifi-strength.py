03dceaf3cb32ca94d8eb43adf274864331ece4d3
Akello (234.48:4) <43887608+DolicaAkelloEgwel@users.noreply.github.com>
2026-08-07 09:58:15 +0000

    success light and a bunch of other changes?

--- m5stack/wifi-strength.py
+++ m5stack/wifi-strength.py
@@ -7,15 +7,18 @@ import requests2
 import json
 import time
 import socket
+import gc
 
-wlan_sta = None
-sta_record = None
+wlan = None
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 def send_data(rssi_data):
     try:
-        sock.sendto(json.dumps(rssi_data), ("192.168.1.240", 5000))
+        sock.sendto(json.dumps(rssi_data), ("192.168.0.107", 8080))
+        Power.setLed(255)
+        time.sleep(0.5)
+        Power.setLed(0)
     except Exception as e:
         print("Send failed:", e)
 
@@ -25,29 +28,37 @@ def format_mac(mac):
 
     
 def setup():
-    global wlan_sta
+    global wlan
     M5.begin()
     Widgets.setRotation(0)
     Widgets.fillScreen(0x000000)
-    wlan_sta = network.WLAN(network.STA_IF)
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    wlan.connect("TP-Link_9734","12327423")
     
 def loop():
-    global wlan_sta
+    global wlan
     M5.update()
-    rssis = {format_mac(record[1]): {"name": record[0].decode("utf-8"), "rssi": record[3]}  for record in wlan_sta.scan() }
-    print(f"Found {len(rssis)} networks in scan.")
-    send_data(rssis)
+    try:
+        rssis = {format_mac(record[1]): {"name": record[0].decode("utf-8"), "rssi": record[3]} for record in wlan.scan()}
+        print(f"Found {len(rssis)} networks in scan.")
+        send_data(rssis)
+    except OSError as e:
+        print(e)
+        gc.collect()
+    
     
 if __name__ == '__main__':
     try:
         setup()
         while True:
             loop()
-            time.sleep(0.1)
+            time.sleep(1)
     except (Exception, KeyboardInterrupt) as e:
         try:
             from utility import print_error_msg
             print_error_msg(e)
         except ImportError:
             print("please update to latest firmware")
+
 
