74efd13478add1708dd7b19213a03e0108a99aef
Akello (234.48:4) <43887608+DolicaAkelloEgwel@users.noreply.github.com>
2026-08-24 15:41:01 +0100

    conversion to int

--- python/strength-range.py
+++ python/strength-range.py
@@ -6,10 +6,12 @@ for name in fnames:
     with open(f"./python/rssi-readings/{name}", "r") as f:
         for line in f:
             name, rssi = line.split(" ")
+            rssi = int(rssi.strip())
             if name not in strength_values:
                 strength_values[name] = [rssi]
             else:
                 strength_values[name].append(rssi)
 
 for key in strength_values:
-    print(key, len(strength_values[key]))
+    strength_values[key].sort()
+    print(key, len(strength_values[key]), strength_values[key][:10], strength_values[key][-10:])
