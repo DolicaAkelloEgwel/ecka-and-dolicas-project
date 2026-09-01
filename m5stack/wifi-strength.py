49f9810955ba373c5a6759c481bc1976417844aa
Akello (234.48:4) <43887608+DolicaAkelloEgwel@users.noreply.github.com>
2026-09-01 15:51:38 +0100

    flash led for wifi collection + buzz when on

--- m5stack/wifi-strength.py
+++ m5stack/wifi-strength.py
@@ -1,24 +1,25 @@
 # main.py
-import os, sys, io
-import M5
-from M5 import *
-import network
-import requests2
-import json
-import time
-import socket
 import gc
+import json
+import socket
+import time
+
+import M5
+import network
+from M5 import *
 
 wlan = None
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+TARGETS = ("eduroam", "UAL-Guest-WiFi", "UAL-IoT", "UAL-WiFi")
 
 
 def send_data(rssi_data):
     try:
         sock.sendto(json.dumps(rssi_data), ("192.168.0.107", 8080))
         Power.setLed(255)
-        time.sleep(0.5)
+        time.sleep(0.1)
         Power.setLed(0)
     except Exception as e:
         print("Send failed:", e)
@@ -34,14 +35,16 @@ def setup():
     M5.Display.setBrightness(0)
     Widgets.setRotation(0)
     Widgets.fillScreen(0x000000)
+    time.sleep(2)
     wlan = network.WLAN(network.STA_IF)
     wlan.active(True)
     wlan.connect("TP-Link_9734", "12327423")
 
 
-def loop():
+def wireless_scan():
     global wlan
     M5.update()
+
     try:
         rssis = {
             format_mac(record[1]): {
@@ -52,6 +55,9 @@ def loop():
         }
         print(f"Found {len(rssis)} networks in scan.")
         send_data(rssis)
+        Power.setLed(255)
+        time.sleep(0.1)
+        Power.setLed(0)
     except OSError as e:
         print(e)
         gc.collect()
@@ -60,9 +66,11 @@ def loop():
 if __name__ == "__main__":
     try:
         setup()
+        Speaker.setVolumePercentage(1)
+        Speaker.tone(2000, 1000)
         while True:
-            loop()
-            time.sleep(1)
+            wireless_scan()
+            time.sleep(0.1)
     except (Exception, KeyboardInterrupt) as e:
         try:
             from utility import print_error_msg
