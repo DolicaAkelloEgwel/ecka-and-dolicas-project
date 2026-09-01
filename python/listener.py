c0d5f6c5070ce0de928a9290d248efc6eb674058
Akello (234.48:4) <43887608+DolicaAkelloEgwel@users.noreply.github.com>
2026-09-01 12:29:25 +0100

    rssirange object

--- python/listener.py
+++ python/listener.py
@@ -10,28 +10,45 @@ sock.bind(("0.0.0.0", 8080))
 print("Listening...")
 
 # track names of nearby wifi networks
-WIFI_NAMES = ("eduroam", "UAL-IoT", "UAL-WiFi", "UAL-Guest-WiFi", "Igloo")
-
-MIN_RECORDINGS = 30
-MIN_NETWORKS_NEEDED = 10
-MAX_SCANS = 200
+WIFI_NAMES = ("eduroam", "UAL-IoT", "UAL-WiFi", "UAL-Guest-WiFi")
 
 
-def calculate_median(vals):
+class RSSIRange:
+    def __init__(self, min_rssi, max_rssi):
 
-    ordered = sorted(vals)
-    n = len(vals)
+        self.min = min_rssi
+        self.max = max_rssi
+        self.diff = abs(self.max - self.min)
+        self.offset = -self.min / self.diff - self.max / self.diff
 
-    if n % 2 == 0:
-        mid_index = n // 2
-        return (ordered[mid_index - 1] + ordered[mid_index]) / 2
-    else:
-        return ordered[(n // 2)]
+    def map(self, value):
+        return (value / diff) * 2 + self.offset
+
+    @staticmethod
+    def send_osc_message(name, val):
+        osc_client.send_message(f"/{name}", val)
 
 
-counter = 0
+rssi_ranges = dict()
+rssi_ranges["UAL-IoT-a4:9b:cd:bf:3b:04"] = RSSIRange(-91, -60)
+rssi_ranges["eduroam-a4:9b:cd:be:ea:25"] = RSSIRange(-92, -63)
+rssi_ranges["UAL-IoT-a4:9b:cd:be:ea:24"] = RSSIRange(-92, -62)
+rssi_ranges["eduroam-a4:9b:cd:bf:01:45"] = RSSIRange(-92, -68)
+rssi_ranges["UAL-IoT-a4:9b:cd:bf:01:44"] = RSSIRange(-93, -68)
+rssi_ranges["eduroam-a4:9b:cd:bf:28:a5"] = RSSIRange(-94, -70)
+rssi_ranges["eduroam-a4:9b:cd:bf:29:65"] = RSSIRange(-94, -75)
+rssi_ranges["UAL-IoT-a4:9b:cd:bf:29:64"] = RSSIRange(-94, -75)
+rssi_ranges["UAL-IoT-a4:9b:cd:bf:9d:c4"] = RSSIRange(-91, -62)
+rssi_ranges["eduroam-a4:9b:cd:bf:9d:c5"] = RSSIRange(-91, -62)
+rssi_ranges["UAL-IoT-a4:9b:cd:bf:28:a4"] = RSSIRange(-95, -70)
+rssi_ranges["eduroam-a4:9b:cd:be:f8:e5"] = RSSIRange(-92, -55)
+rssi_ranges["eduroam-a4:9b:cd:bf:3b:05"] = RSSIRange(-90, -59)
+rssi_ranges["UAL-IoT-a4:9b:cd:be:f8:e4"] = RSSIRange(-93, -55)
+rssi_ranges["eduroam-a4:9b:cd:be:f5:a5"] = RSSIRange(-96, -71)
+rssi_ranges["UAL-IoT-a4:9b:cd:be:f5:a4"] = RSSIRange(-95, -72)
 
-while counter < MAX_SCANS:
+
+while True:
 
     data, addr = sock.recvfrom(2048)
     data = data.decode()
@@ -39,31 +56,11 @@ while counter < MAX_SCANS:
     print("Received data.")
 
     for key in d.keys():
+
         # continue if the network isn't in the list
         if d[key]["name"] not in WIFI_NAMES:
             continue
 
         combined_name = f"{d[key]['name']}-{key}"
 
-        # create a new dictionary entry if this has not been seen before
-        if combined_name not in RSSI_VALUES:
-            RSSI_VALUES[combined_name] = []
-
-        RSSI_VALUES[combined_name].append(d[key]["rssi"])
-
-    counter += 1
-
-    # stop when at least 30 recordings have been taken for at least 10 networks
-    if (
-        sum([len(vals) >= MIN_RECORDINGS for vals in RSSI_VALUES.values()])
-        >= MIN_NETWORKS_NEEDED
-    ):
-        break
-
-    print(RSSI_VALUES)
-
-# calculate the median rssi value
-with open("recordings", "a") as f:
-    for key in RSSI_VALUES:
-        if len(RSSI_VALUES[key]) >= MIN_RECORDINGS:
-            f.write(f"{key}: {calculate_median(RSSI_VALUES[key])}\n")
+        rssi_ranges[combined_name].send_osc_message(combined_name, d[key]["rssi"])
