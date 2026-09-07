47693cf646a44550e357ec071b5d376698eac329
Akello (234.48:4) <43887608+DolicaAkelloEgwel@users.noreply.github.com>
2026-09-07 15:26:53 +0100

    don't repeat network names + private properties + fixing too many arguments

--- python/listener.py
+++ python/listener.py
@@ -29,30 +29,30 @@ class RSSIRange:
     def __init__(self, name, min_rssi, max_rssi):
 
         self._name = name
-        self.min = min_rssi
-        self.max = max_rssi
-        self.diff = abs(self.max - self.min)
-        self.offset = (1 / self.diff) * (-self.min - self.max)
+        self._min = min_rssi
+        self._max = max_rssi
+        self._diff = abs(self._max - self._min)
+        self._offset = (1 / self._diff) * (-self._min - self._max)
 
         if SIMULATE:
-            self.prev = None
+            self._prev = None
 
     def _map(self, value):
-        return 2 * (value / self.diff) + self.offset
+        return 2 * (value / self._diff) + self._offset
 
     def simulate(self):
         if self.prev is None:
-            self.prev = randint(self.min, self.max)
+            self.prev = randint(self._min, self._max)
 
         next_value = self.prev + uniform(-5, 5)
 
-        if next_value < self.min:
-            self.prev = self.min
-            return self.min
+        if next_value < self._min:
+            self.prev = self._min
+            return self._min
 
-        if next_value > self.max:
-            self.prev = self.max
-            return self.max
+        if next_value > self._max:
+            self.prev = self._max
+            return self._max
 
         self.prev = next_value
         return next_value
@@ -61,58 +61,27 @@ class RSSIRange:
         osc_client.send_message(f"/{self._name}", self._map(val))
 
 
-rssi_ranges = dict()
-rssi_ranges["UAL-IoT-a4:9b:cd:bf:3b:04"] = RSSIRange(
-    "UAL-IoT-a4:9b:cd:bf:3b:04", -91, -60
-)
-rssi_ranges["eduroam-a4:9b:cd:be:ea:25"] = RSSIRange(
-    "eduroam-a4:9b:cd:be:ea:25", -92, -63
-)
-rssi_ranges["UAL-IoT-a4:9b:cd:be:ea:24"] = RSSIRange(
-    "UAL-IoT-a4:9b:cd:be:ea:24", -92, -62
-)
-rssi_ranges["eduroam-a4:9b:cd:bf:01:45"] = RSSIRange(
-    "eduroam-a4:9b:cd:bf:01:45", -92, -68
-)
-rssi_ranges["UAL-IoT-a4:9b:cd:bf:01:44"] = RSSIRange(
-    "UAL-IoT-a4:9b:cd:bf:01:44", -93, -68
-)
-rssi_ranges["eduroam-a4:9b:cd:bf:28:a5"] = RSSIRange(
-    "eduroam-a4:9b:cd:bf:28:a5", -94, -70
-)
-rssi_ranges["eduroam-a4:9b:cd:bf:29:65"] = RSSIRange(
-    "eduroam-a4:9b:cd:bf:29:65", -94, -75
-)
-rssi_ranges["UAL-IoT-a4:9b:cd:bf:29:64"] = RSSIRange(
-    "UAL-IoT-a4:9b:cd:bf:29:64", -94, -75
-)
-rssi_ranges["UAL-IoT-a4:9b:cd:bf:9d:c4"] = RSSIRange(
-    "UAL-IoT-a4:9b:cd:bf:9d:c4", -91, -62
-)
-rssi_ranges["eduroam-a4:9b:cd:bf:9d:c5"] = RSSIRange(
-    "eduroam-a4:9b:cd:bf:9d:c5", -91, -62
-)
-rssi_ranges["UAL-IoT-a4:9b:cd:bf:28:a4"] = RSSIRange(
-    "UAL-IoT-a4:9b:cd:bf:28:a4", -95, -70
-)
-rssi_ranges["eduroam-a4:9b:cd:be:f8:e5"] = RSSIRange(
-    "eduroam-a4:9b:cd:be:f8:e5", -92, -55
-)
-rssi_ranges["eduroam-a4:9b:cd:bf:3b:05"] = RSSIRange(
-    "eduroam-a4:9b:cd:bf:3b:05", -90, -59
-)
-rssi_ranges["UAL-IoT-a4:9b:cd:be:f8:e4"] = RSSIRange(
-    "UAL-IoT-a4:9b:cd:be:f8:e4", -93, -55
-)
-rssi_ranges["eduroam-a4:9b:cd:be:f5:a5"] = RSSIRange(
-    "eduroam-a4:9b:cd:be:f5:a5", -96, -71
-)
-rssi_ranges["UAL-IoT-a4:9b:cd:be:f5:a4"] = RSSIRange(
-    "UAL-IoT-a4:9b:cd:be:f5:a4", -95, -72
-)
-rssi_ranges["UAL-IoT-a4:9b:cd:bf:d7:c4"] = RSSIRange(
-    "UAL-IoT-a4:9b:cd:bf:d7:c4", -95, -78
-)
+rssi_ranges = [
+    RSSIRange("UAL-IoT-a4:9b:cd:bf:3b:04", -91, -60),
+    RSSIRange("eduroam-a4:9b:cd:be:ea:25", -92, -63),
+    RSSIRange("UAL-IoT-a4:9b:cd:be:ea:24", -92, -62),
+    RSSIRange("eduroam-a4:9b:cd:bf:01:45", -92, -68),
+    RSSIRange("UAL-IoT-a4:9b:cd:bf:01:44", -93, -68),
+    RSSIRange("eduroam-a4:9b:cd:bf:28:a5", -94, -70),
+    RSSIRange("eduroam-a4:9b:cd:bf:29:65", -94, -75),
+    RSSIRange("UAL-IoT-a4:9b:cd:bf:29:64", -94, -75),
+    RSSIRange("UAL-IoT-a4:9b:cd:bf:9d:c4", -91, -62),
+    RSSIRange("eduroam-a4:9b:cd:bf:9d:c5", -91, -62),
+    RSSIRange("UAL-IoT-a4:9b:cd:bf:28:a4", -95, -70),
+    RSSIRange("eduroam-a4:9b:cd:be:f8:e5", -92, -55),
+    RSSIRange("eduroam-a4:9b:cd:bf:3b:05", -90, -59),
+    RSSIRange("UAL-IoT-a4:9b:cd:be:f8:e4", -93, -55),
+    RSSIRange("eduroam-a4:9b:cd:be:f5:a5", -96, -71),
+    RSSIRange("UAL-IoT-a4:9b:cd:be:f5:a4", -95, -72),
+    RSSIRange("UAL-IoT-a4:9b:cd:bf:d7:c4", -95, -78),
+]
+
+rssi_ranges = {network._name: network for network in rssi_ranges}
 
 if SIMULATE:
     while True:
@@ -147,4 +116,4 @@ while True:
         if combined_name not in rssi_ranges:
             continue
 
-        rssi_ranges[combined_name].send_osc_message(combined_name, d[key]["rssi"])
+        rssi_ranges[combined_name].send_osc_message(d[key]["rssi"])
